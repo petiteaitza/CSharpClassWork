c334e9b8364bb1bda925fbac17f58e1a21a21787
Aitza B <36286103+petiteaitza@users.noreply.github.com>
2018-07-14 13:01:17 -0400

    Add files via upload

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args) // ref pgs 66 &67
+        {
+            int output = factorial(5);
+            Console.WriteLine("factorial of 5:" + output);
+        }
+
+        static int factorial(int num)
+        {
+            int result;
+            if (num == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = (factorial(num - 1) * num); // if you didn't have this end, it would be infinite recursion which would cause a crash
+            }
+            return result;
+        }
+       
+    }
+}
---
+++ overload/overload/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static double computeArea(double width)
+        {
+            double radius = width / 2;
+            return((radius * radius) * 3.14593);
+        }
+    }
+}
