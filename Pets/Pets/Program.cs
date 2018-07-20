98db62fac15c69dfce556e4e0b67137bad89a4f8
Aitza B <36286103+petiteaitza@users.noreply.github.com>
2018-07-19 21:23:04 -0400

    Add files via upload

---
+++ Pets/Pets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Pets
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Dog fido = new Dog();
+            fido.setValues("Fido", 3, "Brown");
+            string tagF = String.Format("{0} is a {1} year old {2} dog",
+                fido.getName(),
+                fido.getAge(),
+                fido.getColor()
+                );
+
+
+            Console.WriteLine(tagF + fido.bark());
+
+            Dog lucy = new Dog();
+            lucy.setValues("Lucy", 2, "Gray");
+            string tagL = String.Format("{0} is a {1} year old {2} dog",
+              lucy.getName(),
+            lucy.getAge(),
+            lucy.getColor()
+
+            );
+              
+
+            Console.WriteLine(tagL + lucy.bark());
+
+
+            Console.ReadKey();
+        }
+        
+      
+        public class Dog
+        {
+            private string name, color;
+            private int age;
+
+            public void setValues(string name, int age, string color)
+            {
+                this.name = name;
+                this.age = age;
+                this.color = color;
+            }
+
+
+            public string getName() { return name; }
+            public int getAge() { return age; }
+            public string getColor() { return color; }
+
+            public string bark()
+            {
+                return "\nWoof, woof!\n";
+            }
+        }
+
+    } 
+   
+}    
