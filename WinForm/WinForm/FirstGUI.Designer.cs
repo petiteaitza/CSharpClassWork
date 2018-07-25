4b492bd78d9a038c72ef5ba98d4b3da95471653d
Aitza B <36286103+petiteaitza@users.noreply.github.com>
2018-07-24 21:10:14 -0400

    Add files via upload

---
+++ WinForm/WinForm/FirstGUI.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WinForm
+{
+    partial class FirstGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNEvents = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTNEvents
+            // 
+            this.BTNEvents.Location = new System.Drawing.Point(12, 24);
+            this.BTNEvents.Name = "BTNEvents";
+            this.BTNEvents.Size = new System.Drawing.Size(107, 38);
+            this.BTNEvents.TabIndex = 0;
+            this.BTNEvents.Text = "Run Events";
+            this.BTNEvents.UseVisualStyleBackColor = true;
+            this.BTNEvents.Click += new System.EventHandler(this.BTNEvents_Click);
+            this.BTNEvents.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BTNEvents_MouseClick);
+            this.BTNEvents.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BTNEvents_MouseDown);
+            this.BTNEvents.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BTNEvents_MouseUp);
+            // 
+            // FirstGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 375);
+            this.Controls.Add(this.BTNEvents);
+            this.Name = "FirstGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FirstGUI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNEvents;
+    }
+}
+
---
+++ WinForm/WinForm/FirstGUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class FirstGUI : Form
+    {
+        public FirstGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNEvents_MouseClick(object sender, MouseEventArgs e)
+        {
+            BTNEvents.BackColor = Color.Fuchsia;
+
+        }
+
+        private void BTNEvents_MouseUp(object sender, MouseEventArgs e)
+        {
+            BTNEvents.BackColor = Color.Aqua;
+
+        }
+
+        private void BTNEvents_MouseDown(object sender, MouseEventArgs e)
+        {
+            BTNEvents.BackColor = Color.Lime;
+        }
+
+        private void BTNEvents_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C# Programming in easy steps");
+        }
+    }
+}
